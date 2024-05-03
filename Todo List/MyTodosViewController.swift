ed927567d22f58dac3da2f6278702368a5b5dd5f
Eugene-kms <ievgenkolomiiets@gmail.com>
2024-05-03 16:05:56 +0200

    Edit My Todos screen, added items for each Todo

--- Todo List/MyTodosViewController.swift
+++ Todo List/MyTodosViewController.swift
@@ -2,9 +2,23 @@ import UIKit
 
 class MyTodosViewController: UIViewController {
     
+    var lists: [TodoList] = []
+    
+    @IBOutlet weak var tableView: UITableView!
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        lists = myTodoLists()
+        configureTableView()
+    }
+    
+    func configureTableView() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        let cellName = "TodoListCell"
+        tableView.register(UINib(nibName: cellName, bundle: nil), forCellReuseIdentifier: cellName)
+        tableView.rowHeight = 44
     }
     
     func present(with todoList: TodoList) {
@@ -15,24 +29,101 @@ class MyTodosViewController: UIViewController {
         present(todoListViewController, animated: true)
     }
     
-    @IBAction func groceriesTapped(_ sender: Any) {
-        present(with: TodoList(
+    private func myTodoLists() -> [TodoList] {
+        var lists = [TodoList]()
+        
+        lists.append(TodoList(
             title: "Croseries",
             image: .avocado,
-            color: .greenTodo))
-    }
-    
-    @IBAction func vacationTapped(_ sender: Any) {
-        present(with: TodoList(
+            color: .greenTodo,
+            items: groceriesItems()))
+        
+        lists.append(TodoList(
             title: "Vacation",
             image: .vacation,
-            color: .redTodo))
-    }
-    
-    @IBAction func choresTapped(_ sender: Any) {
-        present(with: TodoList(
+            color: .redTodo,
+            items: vacationItems()))
+        
+        lists.append(TodoList(
             title: "House Chores",
             image: .chores,
-            color: .blueTodo))
+            color: .blueTodo,
+            items: choresItems()))
+        
+        return lists
+        
+    }
+    
+    private func groceriesItems() -> [String] {
+        var items = [String]()
+        
+        items.append("Whole wheat bread")
+        items.append("Almond milk")
+        items.append("Cage-free eggs")
+        items.append("Fresh spinach")
+        items.append("Greek yogurt")
+        items.append("Quinoa")
+        items.append("Avocados")
+        items.append("Cherry tomatoes")
+        items.append("Organic chicken breast")
+        items.append("Ground turmeric")
+        items.append("Almonds")
+        items.append("Dark chocolate")
+        
+        return items
+    }
+    
+    private func vacationItems() -> [String] {
+        var list = [String]()
+        
+        list.append("Check weather")
+        list.append("Accommodation")
+        list.append("Daily Plan")
+        list.append("Passport and visa requirements")
+        list.append("Arrange pet care")
+        list.append("Exchange currency")
+        list.append("Confirm airport transfers")
+        list.append("Flight tickets")
+        list.append("Restaurant reservations")
+        
+        return list
+    }
+    
+    private func choresItems() -> [String] {
+        var list = [String]()
+        
+        list.append("Vacuum the living room")
+        list.append("Mop kitchen floors")
+        list.append("Clean windows in the dining area")
+        list.append("Dust all surfaces")
+        list.append("Organize the garage")
+        
+        return list
+    }
+}
+
+extension MyTodosViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        lists.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard
+            let cell = tableView.dequeueReusableCell(withIdentifier: "TodoListCell") as? TodoListCell else { return UITableViewCell() }
+        
+        let todoList = lists[indexPath.row]
+        
+        cell.configure(with: todoList)
+        cell.selectionStyle = .none
+        
+        return cell
+    }
+}
+
+extension MyTodosViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let todoList = lists[indexPath.row]
+        present(with: todoList)
     }
 }
--- Todo List/TodoListViewController.swift
+++ Todo List/TodoListViewController.swift
@@ -4,11 +4,13 @@ struct TodoList {
     let title: String
     let image: UIImage
     let color: UIColor
+    let items: [String]
 }
 
 //class for all screen(groceries, vacation, home chores)
 class TodoListViewController: UIViewController {
     
+    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var headerView: UIView!
     @IBOutlet weak var imageBackgroundView: UIView!
     @IBOutlet weak var iconImageView: UIImageView!
@@ -23,6 +25,13 @@ class TodoListViewController: UIViewController {
         imageBackgroundView.layer.masksToBounds = true
         
         configure()
+        configureTableView()
+    }
+    
+    private func configureTableView() {
+        tableView.dataSource = self
+        let cellName = "TodoListItemCell"
+        tableView.register(UINib(nibName: cellName, bundle: nil), forCellReuseIdentifier: cellName)
     }
     
     func configure() {
@@ -33,5 +42,21 @@ class TodoListViewController: UIViewController {
     
     @IBAction func backTapped(_ sender: Any) {
         dismiss(animated: true)
+    }
+}
+
+extension TodoListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        todoList.items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "TodoListItemCell") as? TodoListItemCell else { return UITableViewCell() }
+        
+        let item = todoList.items[indexPath.row]
+        
+        cell.configure(with: item)
+        
+        return cell
     }
 }
