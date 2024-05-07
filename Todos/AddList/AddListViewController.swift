64d98d38323d81c3682200f23278f2e0c868dc18
Eugene-kms <ievgenkolomiiets@gmail.com>
2024-05-07 16:18:56 +0200

    Part III completed

--- Todos/AddList/AddListViewController.swift
+++ Todos/AddList/AddListViewController.swift
@@ -8,6 +8,7 @@ class AddListViewController: UIViewController {
     @IBOutlet weak var imageBackgroundView: UIView!
     @IBOutlet weak var iconImageView: UIImageView!
     @IBOutlet weak var saveBtn: UIButton!
+    @IBOutlet weak var saveBtnBottomConstraint: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,14 +17,49 @@ class AddListViewController: UIViewController {
         saveBtn.setCornerRadius(14)
         
         subscribeToKeyboard()
+        setupHideKeyboardGesture()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        textField.becomeFirstResponder()
+    }
+    
+    private func setupHideKeyboardGesture() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc private func dismissKeyboard() {
+        view.endEditing(true)
+        
     }
     
     private func subscribeToKeyboard() {
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
     @objc private func keyboardWillShow(notification: Notification) {
-        print("keyboardWillShow")
+        
+        guard let userInfo = notification.userInfo,
+              let endFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect,
+              let duration = userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double else { return }
+        //if the keyboard is hidden
+        if endFrame.origin.y >= UIScreen.main.bounds.size.height {
+            saveBtnBottomConstraint.constant = 0
+        } else {//if the keyboard is presented
+            let buttonBottomMargin = 16 + endFrame.height - view.safeAreaInsets.bottom
+            saveBtnBottomConstraint.constant = buttonBottomMargin
+        }
+        
+        view.layoutIfNeeded()
+        
+        UIView.animate(withDuration: duration) {
+            self.view.layoutIfNeeded()
+        }
     }
     
     @IBAction func backTapped(_ sender: Any) {
